fcb5c09674ccdeb3603d1c4ab52208040a2fe434
AyakorK <guillaume.moret@yahoo.com>
2023-10-02 12:13:08 +0200

    Remove unnecessaries comments

--- src/exo1.ts
+++ src/exo1.ts
@@ -1,4 +1,3 @@
-
 import axios from 'axios'
 
 export function getUserSummary(id: number) {
--- src/exo7.ts
+++ src/exo7.ts
@@ -2,22 +2,18 @@ export class Queue<T> {
   private elements: T[] = [];
 
 
-  // Méthode pour ajouter un élément à la file d'attente
   enqueue(item: T) {
     this.elements.push(item);
   }
 
-  // Méthode pour retirer et renvoyer le premier élément de la file d'attente
   dequeue(): T | undefined {
     return this.elements.shift();
   }
 
-  // Méthode pour obtenir la taille de la file d'attente
   size(): number {
     return this.elements.length;
   }
 
-  // Méthode pour vérifier si la file d'attente est vide
   isEmpty(): boolean {
     return this.elements.length === 0;
   }
